5115583c6b28d11f1fdb20fbf365b2783492e67d
Ki Jung Kim <kijungkim@eworldes.com>
2019-04-24 13:22:55 -1000

    fixing reference error

--- db/migrations/20190424120434_create_events_table.js
+++ db/migrations/20190424120434_create_events_table.js
@@ -2,7 +2,8 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('events', (table) => {
     table.increments();
     table.string('description', 256).notNullable().unique();
-    table.foreign('icon').references('id').inTable('icons');
+    table.integer('icon_id')
+    table.foreign('icon_id').references('id').inTable('icons');
     table.timestamps(true, true);
   })
 };
