15cd30226c9e36aa9ee4e48ec124a062b70908e9
Ki Jung Kim <kijungkim@eworldes.com>
2019-04-25 13:25:36 -1000

    fixed typo on route

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const
   webhook = require('./webhook'),
   events = require('./events');
 
-router.use('/event', events);
+router.use('/events', events);
 router.use('/webhook', webhook);
 
 module.exports = router;
