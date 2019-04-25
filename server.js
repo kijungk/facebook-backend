d6a1e9299f14443ad779be38b27fa5df5c70e793
Ki Jung Kim <kijungkim@eworldes.com>
2019-04-25 11:55:42 -1000

    testing hooking up frontend

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
+app.use(express.static(path.join(__dirname, '/dist/facebook-frontend')));
+
 app.use('/api', routes);
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, './dist/facebook-frontend/index.html'), (err) => {
+    if (err) {
+      response.status(500).json(err);
+    }
+  })
+})
 
 module.exports = app;
