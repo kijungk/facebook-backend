409b8130044a4105d3454aa9cc10bc035a23aa05
Ki Jung Kim <kijungkim@eworldes.com>
2019-05-03 11:39:13 -1000

    fixing fetchAll

--- routes/events/index.js
+++ routes/events/index.js
@@ -7,17 +7,17 @@ const
 
 router.route('/')
   .get((request, response) => {
-      //get events list from db
-      return new Event()
-        .fetchAll()
-        .then((events) => {
-          console.log('\n\n\n\n' + events);
-          return response.status(200).json(events);
-        })
-        .catch((error) => {
-          console.log(error);
-          //save to db
-          return;
-        })
-
-      module.exports = router;
+    //get events list from db
+    return new Event()
+      .fetchAll()
+      .then((events) => {
+        console.log('\n\n\n\n' + events);
+        return response.status(200).json(events);
+      })
+      .catch((error) => {
+        console.log(error);
+        //save to db
+        return;
+      })
+  })
+module.exports = router;
